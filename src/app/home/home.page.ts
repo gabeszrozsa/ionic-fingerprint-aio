f2d4d49fe115bb14c1fec35c3f5a4cd10f0657a9
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2022-07-13 14:26:15 +0200

    MVP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private faio: FingerprintAIO, private platform: Platform) {}
 
-  constructor() {}
+  async showFingerprint() {
+    await this.platform.ready();
 
+    try {
+      const available = await this.faio.isAvailable();
+
+      try {
+        const val = await this.faio.show({
+          title: 'Biometric Authentication',
+          subtitle: 'Coolest Plugin ever',
+          description: 'Please authenticate',
+          fallbackButtonTitle: 'Use Backup',
+        });
+        alert('success: ' + JSON.stringify(val));
+      } catch (error) {
+        alert('show error: ' + JSON.stringify(error));
+      }
+    } catch (error) {
+      alert('not available' + JSON.stringify(error));
+    }
+  }
 }
